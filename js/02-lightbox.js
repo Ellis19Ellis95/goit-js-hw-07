97a4143d857839c143d298a513ef72fcb812668c
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-14 20:33:32 +0200

    13

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -26,64 +26,40 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function renderGallery(galleryItems) {
     const galleryElements = galleryItems.slice(0, imagesToDisplay).map(createGalleryItem);
-    galleryList.innerHTML = '';
+    galleryList.innerHTML = ''; 
     galleryList.append(...galleryElements);
   }
 
   renderGallery(galleryItems);
 
-  // Initialize SimpleLightbox on gallery item click
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-    history: true, // Enable navigation with browser history
-    elements: galleryItems.map(({ original, description }) => ({ src: original, caption: description })),
+  
+  galleryList.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const target = event.target;
+
+    if (target.nodeName !== 'IMG') return;
+
+    const largeImageUrl = target.dataset.source;
+
+    
+    const instance = basicLightbox.create(`
+      <img src="${largeImageUrl}" alt="Modal Image">
+    `);
+
+    instance.show();
   });
+  
+  // Add event listeners for next and previous buttons
+nextButton.addEventListener('click', () => lightbox.next());
+prevButton.addEventListener('click', () => lightbox.prev());
+
 });
 
 
 
 
 
-
-document.addEventListener('DOMContentLoaded', function () {
-  const galleryList = document.querySelector('.gallery');
-  const imagesToDisplay = 9;
-
-  function createGalleryItem({ preview, original, description }) {
-    const listItem = document.createElement('li');
-    listItem.classList.add('gallery__item');
-
-    const link = document.createElement('a');
-    link.classList.add('gallery__link');
-    link.href = original;
-
-    const image = document.createElement('img');
-    image.classList.add('gallery__image');
-    image.src = preview;
-    image.setAttribute('data-source', original);
-    image.alt = description;
-
-    link.appendChild(image);
-    listItem.appendChild(link);
-
-    return listItem;
-  }
-
-  function renderGallery(galleryItems) {
-    const galleryElements = galleryItems.slice(0, imagesToDisplay).map(createGalleryItem);
-    galleryList.innerHTML = '';
-    galleryList.append(...galleryElements);
-  }
-
-  renderGallery(galleryItems);
-
-  // Initialize SimpleLightbox on gallery item click
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-  });
-});
 
 
 
