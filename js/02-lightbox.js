bad0b53d54e949d69334a9ddb96a353fa3729f51
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-14 22:00:31 +0200

    21

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 document.addEventListener('DOMContentLoaded', function () {
+  const gallery = document.querySelector('.gallery');
   const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
@@ -8,28 +9,19 @@ document.addEventListener('DOMContentLoaded', function () {
     docClose: true,
   });
 
-  lightbox.on('show.simplelightbox', function () {
-    const prevButton = document.createElement('div');
-    prevButton.className = 'sl-prev';
-    prevButton.innerHTML = '&lt;';
-
-    const nextButton = document.createElement('div');
-    nextButton.className = 'sl-next';
-    nextButton.innerHTML = '&gt;';
-
-    document.body.appendChild(prevButton);
-    document.body.appendChild(nextButton);
-
-    prevButton.addEventListener('click', () => lightbox.prev());
-    nextButton.addEventListener('click', () => lightbox.next());
+  gallery.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (event.target.tagName === 'IMG') {
+      lightbox.open();
+    }
   });
 
-  lightbox.on('close.simplelightbox', function () {
+  lightbox.on('show.simplelightbox', function () {
     const prevButton = document.querySelector('.sl-prev');
     const nextButton = document.querySelector('.sl-next');
 
-    if (prevButton) prevButton.remove();
-    if (nextButton) nextButton.remove();
+    if (prevButton) prevButton.innerHTML = '&lt;';
+    if (nextButton) nextButton.innerHTML = '&gt;';
   });
 });
 
