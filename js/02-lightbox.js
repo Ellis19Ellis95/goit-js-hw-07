909b4984f4d9c3e3ae8465a99a1fc3bd731cd364
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-14 22:39:43 +0200

    26

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 document.addEventListener('DOMContentLoaded', function () {
-  const gallery = document.querySelector('.gallery');
-  const lightbox = new SimpleLightbox('.gallery a', {
+  const gallery = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
     history: true,
@@ -12,17 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
     nav: true,
     buttons: true,
   });
-
-  gallery.addEventListener('click', function (event) {
-    event.preventDefault();
-    if (event.target.tagName === 'IMG') {
-      const src = event.target.getAttribute('src');
-      const alt = event.target.getAttribute('alt');
-      lightbox.open({ source: src, caption: alt });
-    }
-  });
 });
-
 
 
 
