3e68cbff05b67b68f6867a1eba770108ddd7a5da
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-14 21:56:48 +0200

    20

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,54 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 document.addEventListener('DOMContentLoaded', function () {
-  const galleryList = document.querySelector('.gallery');
-  const imagesToDisplay = 9;
-
-  function createGalleryItem({ preview, original, description }) {
-    const listItem = document.createElement('li');
-    listItem.classList.add('gallery__item');
-
-    const link = document.createElement('a');
-    link.classList.add('gallery__link');
-    link.href = original;
-
-    const image = document.createElement('img');
-    image.classList.add('gallery__image');
-    image.src = preview;
-    image.setAttribute('data-source', original);
-    image.alt = description;
-
-    link.appendChild(image);
-    listItem.appendChild(link);
-
-    return listItem;
-  }
-
-  function renderGallery(galleryItems) {
-    const galleryElements = galleryItems.slice(0, imagesToDisplay).map(createGalleryItem);
-    galleryList.innerHTML = '';
-    galleryList.append(...galleryElements);
-  }
-
-  renderGallery(galleryItems);
-
   const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
     history: true,
-    elements: galleryItems.map(({ original, description }) => ({ src: original, caption: description })),
     docClose: true,
   });
 
-  // Add custom navigation buttons
-  const prevButton = document.getElementById('prevButton');
-  const nextButton = document.getElementById('nextButton');
+  lightbox.on('show.simplelightbox', function () {
+    const prevButton = document.createElement('div');
+    prevButton.className = 'sl-prev';
+    prevButton.innerHTML = '&lt;';
 
-  prevButton.addEventListener('click', () => lightbox.prev());
-  nextButton.addEventListener('click', () => lightbox.next());
+    const nextButton = document.createElement('div');
+    nextButton.className = 'sl-next';
+    nextButton.innerHTML = '&gt;';
+
+    document.body.appendChild(prevButton);
+    document.body.appendChild(nextButton);
+
+    prevButton.addEventListener('click', () => lightbox.prev());
+    nextButton.addEventListener('click', () => lightbox.next());
+  });
+
+  lightbox.on('close.simplelightbox', function () {
+    const prevButton = document.querySelector('.sl-prev');
+    const nextButton = document.querySelector('.sl-next');
+
+    if (prevButton) prevButton.remove();
+    if (nextButton) nextButton.remove();
+  });
 });
-
 
 
 
