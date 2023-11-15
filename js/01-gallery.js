7753a2f0ace0add07458133ff28ab3720d228936
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-15 20:24:04 +0200

    36

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,43 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-document.addEventListener('DOMContentLoaded', function () {
-    const galleryList = document.querySelector('.gallery');
-    const imagesToDisplay = 9;
-  
-    function createGalleryItem({ preview, original, description }) {
-      const listItem = document.createElement('li');
-      listItem.classList.add('gallery__item');
-  
-      const link = document.createElement('a');
-      link.classList.add('gallery__link');
-      link.href = original;
-  
-      const image = document.createElement('img');
-      image.classList.add('gallery__image');
-      image.src = preview;
-      image.setAttribute('data-source', original);
-      image.alt = description;
-  
-      link.appendChild(image);
-      listItem.appendChild(link);
-  
-      return listItem;
-    }
 
-
-    //document.addEventListener('DOMContentLoaded', function () {
-        //const galleryList = document.querySelector('.gallery');
-        //const imagesToDisplay = 9;
+    document.addEventListener('DOMContentLoaded', function () {
+        const galleryList = document.querySelector('.gallery');
+        const imagesToDisplay = 9;
       
-        //function createGalleryItem({ preview, original, description }) {
-          //return `
-            //<li class="gallery__item">
-             // <a class="gallery__link" href="${original}">
-               //<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
-              //</a>
-            //</li>
-          //`;
-        //}
+        function createGalleryItem({ preview, original, description }) {
+          return `
+            <li class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+              </a>
+            </li>
+          `;
+        }
       
         function renderGallery(galleryItems) {
           const galleryElements = galleryItems.slice(0, imagesToDisplay).map(createGalleryItem).join('');
