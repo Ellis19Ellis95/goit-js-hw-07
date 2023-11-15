8f2b978f61ba103f7b3695580aa93b16a8bf12d9
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-15 20:27:50 +0200

    38

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,45 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const galleryList = document.querySelector('.gallery');
-        const imagesToDisplay = 9;
-      
-        function createGalleryItem({ preview, original, description }) {
-          return `
-            <li class="gallery__item">
-              <a class="gallery__link" href="${original}">
-                <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
-              </a>
-            </li>
-          `;
-        }
-      
-        function renderGallery(galleryItems) {
-          const galleryElements = galleryItems.slice(0, imagesToDisplay).map(createGalleryItem).join('');
-          galleryList.innerHTML = galleryElements;
-        }
-      
-        renderGallery(galleryItems);
-      
-        galleryList.addEventListener('click', function (event) {
-          event.preventDefault();
-      
-          const target = event.target;
-      
-          if (target.nodeName !== 'IMG') return;
-      
-          const largeImageUrl = target.dataset.source;
-      
-          const instance = basicLightbox.create(`
-            <img src="${largeImageUrl}" alt="Modal Image">
-          `);
-      
-          instance.show();
-        });
-      });
+document.addEventListener('DOMContentLoaded', function () {
+    const galleryList = document.querySelector('.gallery');
+    const imagesToDisplay = 9;
   
-    
+    function createGalleryItem({ preview, original, description }) {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+          </a>
+        </li>
+      `;
+    }
+  
+    function renderGallery(galleryItems) {
+      const galleryElements = galleryItems.slice(0, imagesToDisplay).map(createGalleryItem).join('');
+      galleryList.innerHTML = galleryElements;
+    }
+  
+    renderGallery(galleryItems);
+  
+    galleryList.addEventListener('click', function (event) {
+      event.preventDefault();
+  
+      const target = event.target;
+  
+      if (target.nodeName !== 'IMG') return;
+  
+      const largeImageUrl = target.dataset.source;
+  
+      const instance = basicLightbox.create(`
+        <img src="${largeImageUrl}" alt="Modal Image">
+      `);
+  
+      instance.show();
+    });
+  });
   
     
 
