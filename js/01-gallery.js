7c2c0486ba4c4c20be6f9bd69de54a24d68fae0d
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-15 20:20:55 +0200

    33

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -23,33 +23,49 @@ document.addEventListener('DOMContentLoaded', function () {
   
       return listItem;
     }
-  
-    function renderGallery(galleryItems) {
-      const galleryElements = galleryItems.slice(0, imagesToDisplay).map(createGalleryItem);
-      galleryList.innerHTML = ''; 
-      galleryList.append(...galleryElements);
-    }
-  
-    renderGallery(galleryItems);
+
+
+    //document.addEventListener('DOMContentLoaded', function () {
+        //const galleryList = document.querySelector('.gallery');
+        //const imagesToDisplay = 9;
+      
+        //function createGalleryItem({ preview, original, description }) {
+          //return `
+            //<li class="gallery__item">
+             // <a class="gallery__link" href="${original}">
+               //<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+              //</a>
+            //</li>
+          //`;
+        //}
+      
+        function renderGallery(galleryItems) {
+          const galleryElements = galleryItems.slice(0, imagesToDisplay).map(createGalleryItem).join('');
+          galleryList.innerHTML = galleryElements;
+        }
+      
+        renderGallery(galleryItems);
+      
+        galleryList.addEventListener('click', function (event) {
+          event.preventDefault();
+      
+          const target = event.target;
+      
+          if (target.nodeName !== 'IMG') return;
+      
+          const largeImageUrl = target.dataset.source;
+      
+          const instance = basicLightbox.create(`
+            <img src="${largeImageUrl}" alt="Modal Image">
+          `);
+      
+          instance.show();
+        });
+      });
   
     
-    galleryList.addEventListener('click', function (event) {
-      event.preventDefault();
   
-      const target = event.target;
-  
-      if (target.nodeName !== 'IMG') return;
-  
-      const largeImageUrl = target.dataset.source;
-  
-      
-      const instance = basicLightbox.create(`
-        <img src="${largeImageUrl}" alt="Modal Image">
-      `);
-  
-      instance.show();
-    });
-  });
+    
 
 
 
