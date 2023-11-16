7f91804617b892836ca82771ef6ad366af886cf9
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-16 18:37:54 +0200

    51

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,37 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-  document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     const galleryList = document.querySelector('.gallery');
     const imagesToDisplay = 9;
   
-    function createGalleryItem({ preview, original, description }) {
-      return `
-        <li class="gallery__item">
-          <a class="gallery__link" href="${original}">
-            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
-          </a>
-        </li>
-      `;
-    }
+    const galleryItems = [
+      // Ось тут має бути масив об'єктів з даними для галереї
+    ];
   
-    function renderGallery(galleryItems) {
-      const galleryElements = galleryItems.slice(0, imagesToDisplay).map(createGalleryItem).join('');
-      galleryList.innerHTML = '';
-      galleryList.insertAdjacentHTML('beforeend', galleryElements);
-    }
+    const galleryElements = galleryItems.slice(0, imagesToDisplay).map(({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </li>
+    `).join('');
   
-    renderGallery(galleryItems);
+    galleryList.insertAdjacentHTML('beforeend', galleryElements);
   
     galleryList.addEventListener('click', function (event) {
       event.preventDefault();
-  
       const target = event.target;
   
       if (target.nodeName !== 'IMG') return;
   
       const largeImageUrl = target.dataset.source;
-  
+      
       const instance = basicLightbox.create(`
         <img src="${largeImageUrl}" alt="Modal Image">
       `);
@@ -39,3 +33,4 @@ import { galleryItems } from './gallery-items.js';
       instance.show();
     });
   });
+
