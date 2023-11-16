6636f9c8ad0fc74a8d1e2be534fcf113a694d805
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-16 19:17:36 +0200

    56

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,14 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 document.addEventListener('DOMContentLoaded', function () {
-  const gallery = new SimpleLightbox('.gallery a', {
+  const galleryList = document.querySelector('.gallery');
+  const imagesToDisplay = 9;
+
+  let galleryMarkup = '';
+
+  galleryItems.slice(0, imagesToDisplay).forEach(({ preview, original, description }) => {
+    galleryMarkup += `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+    `;
+  });
+
+  galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+  const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
-    history: true,
-    docClose: true,
-    close: true,
-    nav: true,
-    buttons: true,
   });
 });
- 
